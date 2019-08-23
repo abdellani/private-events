ce3ab2def0c7d3de1157787ecb97356cbad809e8
abdellani <abdellani@gmail.com>
2019-08-23 22:04:10 +0100

    Add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user,:logged_in?
+
+  def logged_in?
+    current_user != nil
+  end
+  def current_user
+    if user_id = session[:user_id]
+      @current_user ||=User.find_by(id:session[:user_id])
+    else
+      nil
+    end
+  end
+  def current_user= user
+    @current_user = user
+  end
+
 end
